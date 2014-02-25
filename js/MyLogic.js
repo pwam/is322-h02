5de328f53e9f712ab599bb33a868db970fa5050f
pwam <gardineerw@gmail.com>
2014-02-25 10:00:26 -0500

    Added the OnDragStart method

--- js/MyLogic.js
+++ js/MyLogic.js
@@ -3,3 +3,24 @@ container.bind(“dragstart”, $.proxy(dragview.OnDragStart, dragview));
 container.bind(“drag”, $.proxy(dragview.OnDrag, dragview));
 container.bind(“dragend”, $.proxy(dragview.OnDragEnd, dragview));
 setInterval($.proxy(dragview.WatchDrag, dragview), 10);
+
+this.onDragStart = function(event) {
+	var touches = event.originalEvent.touches || [event.originalEvent];
+	for(var t=0; t<touches.length; t++) {
+		var el = touches[t].target.parentNode;
+		if(el.className.search('polaroid') > -1) {
+			el = touches[t].target.parentNode.parentNode;
+		}
+		el.style.zIndex = zIndexBackup + 1;
+		zIndexBackup = zIndexBackup + 1;
+
+		if(el && el == this.target) {
+			$(el).children().toggleClass('upSky');
+			this.lastDrag = {
+					el: el,
+					pos: event.touches[t]
+			};
+		return;
+		}
+	}
+}
