ad273f6e84d694277ea23d2f71ceb191d7285029
pwam <gardineerw@gmail.com>
2014-02-25 09:47:50 -0500

    Revert "Added logic for drag events"
    This reverts commit 2f09274a8de63cb00664e787cdced38469bd21aa.

--- js/MyLogic.js
+++
@@ -1,5 +0,0 @@
-var dragview = new DragView($(container));
-container.bind("dragstart", $.proxy(dragview.OnDragStart, dragview));
-container.bind("drag", $.proxy(dragview.OnDrag, dragview));
-container.bind("dragend", $.proxy(dragview.OnDragEnd, dragview));
-SetInterval($.proxy(dragview.WatchDrag, dragview), 10);
